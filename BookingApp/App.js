bc9bed99471c18f12a9e0b89adf936301b836672
nghiabt48 <93903874+nghiabt48@users.noreply.github.com>
2023-05-16 18:05:16 +0700

    commit test

--- BookingApp/App.js
+++ BookingApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>First Test</Text>
+      <Text>First Test 123</Text>
       <StatusBar style="auto" />
     </View>
   );
