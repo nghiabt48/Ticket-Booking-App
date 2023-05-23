7880f80fc0a9630378d243040cb813ec7dec8bf4
HuyGaNe <117162688+HuyGaNe@users.noreply.github.com>
2023-05-23 17:38:03 +0700

    huy

--- BookingApp/App.js
+++ BookingApp/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/component/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>First Test2</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Login></Login>
   );
 }
 
