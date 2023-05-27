a2261b9a49b4dfd3d0c7a25185ff13ca9c320fdd
caohieplk <133406108+caohieplk@users.noreply.github.com>
2023-05-27 17:43:35 +0700

    hi

--- BookingApp/App.js
+++ BookingApp/App.js
@@ -4,17 +4,19 @@ import Login from './src/Login';
 import { AppConTextProvider } from './src/AppConText';
 import AppNavigator from './src/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import ItemMovi from './src/ItemMovi';
+import ListMovi from './src/ListMovi';
 
 
 
 export default function App() {
   return (
-    <AppConTextProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </AppConTextProvider>
-    
+    // <AppConTextProvider>
+    //   <NavigationContainer>
+    //     <AppNavigator />
+    //   </NavigationContainer>
+    // </AppConTextProvider>
+   <ListMovi/>
   );
 }
 
