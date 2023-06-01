8dc73bc93fb9b9685006e25e018f0ae741d8cb7a
Nha0127 <117083457+Nha0127@users.noreply.github.com>
2023-06-01 17:43:34 +0700

    test đổi mk

--- BookingApp/src/Test1.js
+++ BookingApp/src/Test1.js
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, TextInput, ToastAndroid, View } from 'react-native'
+import React, { useContext, useState } from 'react'
+import AxiosIntance from './AxiosIntance';
+import { AppConText } from './AppConText';
 
 const Test1 = () => {
+  const [Password, setPassword] = useState("");
+  const [newPassword, setnewPassword] = useState("");
+  const [newPasswordConfirn, setnewPasswordConfirn] = useState("");
+  const { setisLogin} = useContext(AppConText);
+  const ChangePassword = async () => {
+    const respone = await AxiosIntance().patch("/users/change-password", { password: Password, newpassword: newPassword,passwordconfirm: newPasswordConfirn});
+    console.log(respone);
+    if (respone.status =="success") {
+        ToastAndroid.show("Đổi mật khẩu thành công", ToastAndroid.SHORT);
+        setisLogin(false)
+    } else {
+        ToastAndroid.show("Doi mat khau that bai", ToastAndroid.SHORT);
+    }
+}
   return (
-    <View>
-      <Text>Test1</Text>
+    <View style={styles.container}>
+      <Text >Mat khau cu</Text>
+      <TextInput style={styles.edt} onChangeText={setPassword}></TextInput>
+      <Text>Mat khau moi</Text>
+      <TextInput style={styles.edt} onChangeText={setnewPassword}></TextInput>
+      <Text>nhap lai mat khau mới</Text>
+      <TextInput style={styles.edt} onChangeText={setnewPasswordConfirn}></TextInput>
+      <Button title='doi mk' onPress={ChangePassword}></Button>
     </View>
   )
 }
 
 export default Test1
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    padding:20,
+  },
+  edt:{
+    borderWidth:1,
+    height:48,
+  }
+})
