e615878d8d257e3eb534617db4a3c050b3000cf8
Nha0127 <117083457+Nha0127@users.noreply.github.com>
2023-06-03 18:19:26 +0700

    Tìm theo tên

--- BookingApp/src/ListMovi.js
+++ BookingApp/src/ListMovi.js
@@ -6,36 +6,65 @@ import AxiosIntance from './AxiosIntance';
 const ListMovi = (props) => {
     const { navigation } = props;
     const [data, setdata] = useState([])
+    const [isLoading, setisLoading] = useState(true);
+    
     useEffect(() => {
         const getDs = async () => {
             const response = await AxiosIntance().get("/movies");
-            if (response.status =="success") {
-              setdata(response.data.data);
-            //   console.log(response.data.data)
+            if (response.status == "success") {
+                setdata(response.data.data);
+                setisLoading(false)
+                //   console.log(response.data.data)
             } else {
-              ToastAndroid.show("Lay du lieu that bai", ToastAndroid.SHORT);
-              
+                ToastAndroid.show("Lay du lieu that bai", ToastAndroid.SHORT);
+
             }
         }
         getDs();
-      return () => {
-       
-      }
+        return () => {
+
+        }
     }, [])
-    
+    let timeOut = null;
+    const down = (searchText) => {
+        if (timeOut) {
+            clearTimeout(timeOut);
+        }
+        timeOut = setTimeout(() => {
+            search(searchText);
+        }, 1000);
+    }
+    const search = async (searchText) => {
+        setisLoading(true);
+        const respone = await AxiosIntance().get("/movies/search?title=" + searchText);
+        if (respone.status == "success") {
+            setdata(respone.movie);
+            setisLoading(false);
+        } else {
+            ToastAndroid.show("Lay du lieu that bai", ToastAndroid.SHORT);
+        }
+    }
     return (
         <View style={styles.container}>
             <View style={styles.Tim}>
-                <TextInput style={styles.TextInputTim}></TextInput>
-                <Image source={require('./image/tim.png')} style={styles.ImageTim} />
+                <TextInput placeholder='Search...' onChangeText={(text) => down(text)} style={styles.TextInputTim}></TextInput>
+                
             </View>
-            <FlatList
-                data={data}
-                numColumns = {2}
-                renderItem={({ item }) => <ItemMovi dulieu={item} navigation={navigation} />}
-                keyExtractor={item => item._id}
-                showsVerticalScrollIndicator={false}
-            />
+            {
+                isLoading == true ? (
+                    <View>
+                        <Text>Loading...</Text>
+                    </View>
+                ) : (
+                    <FlatList
+                        data={data}
+                        numColumns={2}
+                        renderItem={({ item }) => <ItemMovi dulieu={item} navigation={navigation} />}
+                        keyExtractor={item => item._id}
+                        showsVerticalScrollIndicator={false}
+                    />
+                )
+            }
         </View>
     )
 }
@@ -45,28 +74,29 @@ export default ListMovi
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor:'#130B2B',
-        padding:15,
+        backgroundColor: '#130B2B',
+        padding: 15,
     },
-    TextInputTim:{
-        width:'85%',
-        borderWidth:1,
-        borderColor:'#DF0A64',
+    TextInputTim: {
+        width:'100%',
+        borderWidth: 1,
+        borderColor: '#DF0A64',
         padding: 8,
         borderRadius: 20,
-        marginTop: 10,
         color: '#ffff',
-        marginStart:10,
-    },
-    Tim:{
-        flexDirection: 'row',
-        alignItems:'center',
-    },
-    ImageTim:{
-        width:35,
-        height:35,
-        marginLeft:10,
         
+        
+    },
+    Tim: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding:10,
+    },
+    ImageTim: {
+        width: 35,
+        height: 35,
+        marginLeft: 10,
+
     }
-    
+
 })
