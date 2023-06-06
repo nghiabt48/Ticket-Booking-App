d7d15d8a0c5925cbccbfe7f228a4d7a37d646b9f
dunghoang-cre <dunghaps1321@gmail.com>
2023-06-06 17:57:37 +0700

    testing

--- BookingApp/src/Register.js
+++ BookingApp/src/Register.js
@@ -57,7 +57,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        padding: 20
+        padding: 20,
+        backgroundColor: 'rgb(19,11,43)',
+        flexDirection: 'column'
     },
     TextInputUser: {
         borderWidth: 2
